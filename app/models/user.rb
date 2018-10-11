47b1291822524d5b0a655288eae48a6bcdf2078a
pione30 <g.pionelish30@gmail.com>
2018-10-12 01:01:22 +0900

    9.1.1 User. を消すの忘れてたし remember はインスタンスメソッドだった

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,23 +12,23 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
   class << self
 
     # 渡された文字列のハッシュ値を返す
-    def User.digest(string)
+    def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
 
-    def User.new_token
+    def new_token
       SecureRandom.urlsafe_base64
-    end
-
-    # 永続セッションのためにユーザーをデータベースに記憶する
-    def remember
-      self.remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
     end
 
   end
