1e693eef040bbf83ae3a8dd0d9fdc3e9450e06bd
pione30 <g.pionelish30@gmail.com>
2018-10-16 00:28:28 +0900

    9.1.3 forget users

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,11 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   class << self
 
     # 渡された文字列のハッシュ値を返す
