e81d852bbe008cd0a392a8793dd3dcaa82e89f73
pione30 <g.pionelish30@gmail.com>
2018-11-03 01:05:52 +0900

    11.1.2 AccountActivation data model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -43,4 +44,15 @@ class User < ApplicationRecord
     end
 
   end
+
+  private
+
+    def downcase_email
+      email.downcase!
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
