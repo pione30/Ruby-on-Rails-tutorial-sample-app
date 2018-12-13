a81058fb3916a02a14edee0e4dc710d918175296
pione30 <g.pionelish30@gmail.com>
2018-12-13 23:55:10 +0900

    13.4.2 add picture validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,13 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "Should be less than 5 MB")
+      end
+    end
 end
