11ad10e5142526b2cc5fab57905d706e47a47b26
pione30 <g.pionelish30@gmail.com>
2018-12-26 23:54:15 +0900

    14.3.3 subselect

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,8 +68,10 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # ユーザのステータスフィードを返す
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
